04097dec4b278be344873975a331214e9e344068
Sapna Sivakumar <sivakumarsapna@gmail.com>
2024-01-17 13:42:33 +0000

    clean up

--- Arrays/arrayMap.js
+++ Arrays/arrayMap.js
@@ -6,35 +6,3 @@ function findGreaterElements (arr) {
     return isGreaterArray;
 }
 module.exports = findGreaterElements;
-
-//findGreaterElements(array);
-
-
-
-
-//
-// const sumOfNumbers = array.reduce((accumulator, currentValue) => accumulator+currentValue, 0);
-// console.log(sumOfNumbers);
-//
-// const isEven = array.filter((element) => element % 2 === 0 );
-// console.log(isEven);
-//
-// const stringArray = ["cat", "space", "tomato", "is", "done", "telephone"];
-//
-// const fiveLettersArray = stringArray.filter((word) => word.length >=5 ? word : false );
-// console.log(fiveLettersArray);
-//
-//
-// const sumOfEven = array
-//                                 .filter((element) => element % 2 ===0 )
-//                                 .reduce((acc, currentValue) => acc+currentValue, 0);
-//
-// console.log(sumOfEven);
-//
-//
-// let text = "Hello World!";
-// let upper = text.toUpperCase();
-// console.log(upper)
-//
-// const upperCaseArray = stringArray.map((word) => word.toUpperCase());
-// console.log(upperCaseArray);
